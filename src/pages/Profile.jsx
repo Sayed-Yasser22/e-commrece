eec64d5862d18cebcd573ae2f217962c92a016f2
Sayed-Yasser22 <sayedyasser3330@gmail.com>
2026-02-11 10:39:08 +0200

    sayed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,18 +5,14 @@ export default function Profile() {
     <>
      <div className="min-h-screen bg-[#F5F5F5] pb-10">
       
-      {/* 1. الخلفية (Hero Background) */}
       <div 
         className="h-62.5 w-full bg-cover bg-center relative">
     
         <img className='h-62.5 w-full bg-cover bg-center relative' src='/public/533643aa8db82414f48d43a992d009dda3961386.png'></img>
-        {/* لو حابب تضيف أي نص فوق الصورة ممكن هنا */}
       </div>
 
-      {/* حاوية المحتوى اللي في النص */}
       <div className="max-w-150 mx-auto px-4">
         
-        {/* 2. الصورة الشخصية (Avatar) */}
         <div className="relative -mt-16 mb-8 flex justify-center">
           <div className="relative group">
             <img 
@@ -24,7 +20,7 @@ export default function Profile() {
               alt="Profile avatar" 
               className="w-32 h-32 rounded-full border-[5px] border-white object-cover shadow-md"
             />
-            {/* زرار التعديل الصغير */}
+
             <button className="absolute bottom-1 right-1 bg-[#D81B60] text-white p-2 rounded-full border-2 border-white shadow-sm hover:bg-[#b0134d] transition-colors">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
